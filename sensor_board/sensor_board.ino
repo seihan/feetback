ee9e107f409596a60fd29876ed9ebb1afa41ff41
Stefan Nuernberger <kabelfrickler@gmail.com>
2017-07-14 03:05:07 +0200

    Add basic message format/protocol header.
    Using symlinks for common files to please the Arduino IDE.
    
    Signed-off-by: Stefan Nuernberger <kabelfrickler@gmail.com>

--- sensor_board/sensor_board.ino
+++ sensor_board/sensor_board.ino
@@ -1,3 +1,5 @@
+/*
+
 # -Sensor Board-
 # board: Adafruit Bluefruit nRF52 Feather central / peripheral bluetooth low energy (ble)
 
@@ -21,3 +23,38 @@
 # 8x8 sensor matrix input
 
 # transmit 64 sensor values via ble UART or custom service
+
+*/
+
+
+#include <Arduino.h>
+#include "protocol.h"
+
+static struct message_t msg;
+
+void setup() {
+  Serial.begin(9600);
+
+  // 'Clear' the message buffer
+  msg.length = MAX_VALUES;
+  for (uint16_t& val : msg.data) {
+    val = 0xdead;
+  }
+}
+
+void loop() {
+  // read sensor values into message buffer
+  // TODO: remove this fake delay
+  delay(10);
+
+  // compute buffer size to transmit
+  // TODO: this check will be moved to some common code path later
+  if (msg.length > MAX_VALUES) {
+    msg.length = MAX_VALUES;
+  }
+  unsigned length = (msg.length * 2) + 3;
+
+  // write message to serial out
+  Serial.write(reinterpret_cast<const char *>(&msg), length);
+}
+
