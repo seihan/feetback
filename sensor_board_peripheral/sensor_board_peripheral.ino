ddf3838f240110bd3334fe1969885f448523d677
seihan <greenredact@gmail.com>
2017-08-03 13:53:59 +0200

    in progress

--- sensor_board_peripheral/sensor_board_peripheral.ino
+++ sensor_board_peripheral/sensor_board_peripheral.ino
@@ -1,24 +1,21 @@
+#include <bluefruit.h>
+
 int duration = 0;
 
 // 2d array to store values
-int values[16][5] = {};
+int values[16][5] = {}; //[rows][columns]
 
 //mux control pins
-int s0 = 16;
-int s1 = 15;
+int s0 = 15;
+int s1 = 7;
 int s2 = 11;
 int s3 = 31;
 
 //mux in "SIG" pin
-int SIG_pin = 30;
+int SIG_pin = A3;
 
-//analog input pins
-const int columnPins[5] = {
-  A0, A1, A2, A3, A4
-};
 
-//Set channel HIGH
-void writeMux(int channel){
+int readMux(int channel){
   int controlPin[] = {s0, s1, s2, s3};
 
   int muxChannel[16][4]={
@@ -45,25 +42,34 @@ void writeMux(int channel){
     digitalWrite(controlPin[i], muxChannel[channel][i]);
   }
 
-  //turn on the SIG pin
-  //digitalWrite(SIG_pin, HIGH);
+  //read the value at the SIG pin
+  int val = analogRead(SIG_pin);
+
   //return the value
- // return val;
+  return val;
 }
 
-void setup(){
+// BLE Service
+BLEDis  bledis;
+BLEUart bleuart;
+BLEBas  blebas;
+
+// Software Timer for blinking RED LED
+SoftwareTimer blinkTimer;
+
+void setup()
+{
   pinMode(s0, OUTPUT); 
   pinMode(s1, OUTPUT); 
   pinMode(s2, OUTPUT); 
-  pinMode(s3, OUTPUT); 
-  pinMode(SIG_pin, OUTPUT); 
+  pinMode(s3, OUTPUT);
+  pinMode(SIG_pin, INPUT); 
 
   digitalWrite(s0, LOW);
   digitalWrite(s1, LOW);
   digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
-  digitalWrite(SIG_pin, HIGH);
-
+  
   //initialize array with zeros
   for(int i = 0; i < 16; i++){
     for(int j = 0; j < 5; j++){
@@ -72,36 +78,191 @@ void setup(){
    }
 
   Serial.begin(115200);
+  Serial.println("Bluefruit52 BLEUART Example");
+
+  // Initialize blinkTimer for 1000 ms and start it
+  blinkTimer.begin(1000, blink_timer_callback);
+  //blinkTimer.start();
+
+  // Setup the BLE LED to be enabled on CONNECT
+  // Note: This is actually the default behaviour, but provided
+  // here in case you want to control this LED manually via PIN 19
+  Bluefruit.autoConnLed(true);
+
+  Bluefruit.begin();
+  Bluefruit.setName("SensorSoleRight");
+  Bluefruit.setConnectCallback(connect_callback);
+  Bluefruit.setDisconnectCallback(disconnect_callback);
+
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather52");
+  bledis.begin();
+
+  // Configure and Start BLE Uart Service
+  bleuart.begin();
+
+  // Start BLE Battery Service
+  blebas.begin();
+  blebas.write(100);
+
+  // Set up Advertising Packet
+  setupAdv();
+
+  // Start Advertising
+  Bluefruit.Advertising.start();
+
+  Serial.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
+  Serial.println("Once connected, enter character(s) that you wish to send");
 }
 
-void loop(){
+void setupAdv(void)
+{
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include bleuart 128-bit uuid
+  Bluefruit.Advertising.addService(bleuart);
+
+  // There is no room for Name in Advertising packet
+  // Use Scan response for Name
+  Bluefruit.ScanResponse.addName();
+}
+
+
+
+void connect_callback(void)
+{
+  Serial.println("Connected");
+}
+
+void disconnect_callback(uint8_t reason)
+{
+  (void) reason;
+
+  Serial.println();
+  Serial.println("Disconnected");
+  Serial.println("Bluefruit will auto start advertising (default)");
+}
+
+/**
+ * Software Timer callback is invoked via a built-in FreeRTOS thread with
+ * minimal stack size. Therefore it should be as simple as possible. If
+ * a periodically heavy task is needed, please use Scheduler.startLoop() to
+ * create a dedicated task for it.
+ * 
+ * More information http://www.freertos.org/RTOS-software-timer.html
+ */
+void blink_timer_callback(TimerHandle_t xTimerID)
+{
+  (void) xTimerID;
+  digitalToggle(LED_RED);
+}
+
+/**
+ * RTOS Idle callback is automatically invoked by FreeRTOS
+ * when there are no active threads. E.g when loop() calls delay() and
+ * there is no bluetooth or hw event. This is the ideal place to handle
+ * background data.
+ * 
+ * NOTE: It is recommended to call waitForEvent() to put MCU into low-power mode
+ * at the end of this callback. You could also turn off other Peripherals such as
+ * Serial/PWM and turn them back on if wanted
+ * 
+ * e.g
+ * 
+ * void rtos_idle_callback(void)
+ * {
+ *    Serial.stop(); // will lose data when sleeping
+ *    waitForEvent();
+ *    Serial.begin(115200); 
+ * }
+ * 
+ * NOTE2: If rtos_idle_callback() is not defined at all. Bluefruit will force
+ * waitForEvent() to save power. If you don't want MCU to sleep at all, define
+ * an rtos_idle_callback() with empty body !
+ * 
+ * WARNING: This function MUST NOT call any blocking FreeRTOS API 
+ * such as delay(), xSemaphoreTake() etc ... for more information
+ * http://www.freertos.org/a00016.html
+ */
+void rtos_idle_callback(void)
+{
+  // Don't call any other FreeRTOS blocking API()
+  // Perform background task(s) here
+
+  // Request CPU to enter low-power mode until an event/interrupt occurs
+  waitForEvent();
+}
+
+void loop()
+{
   //read values
   duration = millis();
-  for(int i = 0; i < 16; i ++){//rows
-    writeMux(i); //set row HIGH
-    for(int j = 0; j < 5; j++){//columns
-      values[ i ][ j ] = analogRead(columnPins[j]);
+  for(int i = 0; i < 5; i++){
+    //digitalWrite(columnPins[i], HIGH);
+    for(int j = 0; j < 16; j++){
+      values[ j ][ i ] = readMux(j);
+      delay(1);
     }
+    //digitalWrite(columnPins[i], LOW);
   }
   duration = millis() - duration;
-  //print values on serial
-  for(int i = 0; i < 16; i++){
-    for(int j = 0; j < 5; j++){
-      Serial.print (values[ i ][ j ] );
-      Serial.print("\t");
+
+  String frontback = String(String(values[0][0], DEC) + String(",") + String(values[15][0]) + String("|"));
+// Define 
+//String str = "This is my string"; 
+
+// Length (with one extra character for the null terminator)
+int frontback_len = frontback.length() + 1; 
+
+// Prepare the character array (the buffer) 
+char char_array[frontback_len];
+
+// Copy it over 
+frontback.toCharArray(char_array, frontback_len);
+//Serial.println(char_array);
+  bleuart.write(char_array, sizeof(char_array));
+  delay(300);
+  //write values via serial to processing
+      /*Serial.print (values[ i ][ j ], DEC );
+      if(j == 4){
+        Serial.print(";");
+      }
+      else{
+        Serial.print(",");
+      }
     }
-    Serial.println();
-  }
-  Serial.print("duration = ");
-  Serial.print(duration);
-  Serial.println("ms ---------------------");
-  delay(1000);
-   for(int i = 0; i < 16; i++){
+    if(i == 15){
+      Serial.print("|");
+    }
+  }*/
+
+  //reset values to zero
+  for(int i = 0; i < 16; i++){
     for(int j = 0; j < 5; j++){
       values[i][j] = 0;
       }
    }
+  // Forward data from HW Serial to BLEUART
+/*  while (Serial.available())
+  {
+    // Delay to wait for enough input, since we have a limited transmission buffer
+    delay(2);
+
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    bleuart.write( buf, count );
+  }
+
+  // Forward from BLEUART to HW Serial
+  while ( bleuart.available() )
+  {
+    uint8_t ch;
+    ch = (uint8_t) bleuart.read();
+    Serial.write(ch);
+  }*/
+
+  // Request CPU to enter low-power mode until an event/interrupt occurs
+  waitForEvent();
 }
-
-
-
