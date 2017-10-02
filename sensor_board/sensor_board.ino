8b2d13d188c5259de353037f2134221ddc243e2f
Hannes <hannes@localhost.localdomain>
2017-10-02 11:27:17 +0200

    sensor read, serial write

--- sensor_board/sensor_board.ino
+++ sensor_board/sensor_board.ino
@@ -1,7 +1,16 @@
+#define MAX_VALUES 80
+#define ROWS 16
+#define COLUMNS 5
+
+#include "protocol.h"
+
+static struct message_t msg;
 int duration = 0;
 
-// 2d array to store values
-int values[16][5] = {}; //[rows][columns]
+// column pins to set column high
+const int columnPins[ COLUMNS ] = {
+  27, 30, 16, 6, 20
+};
 
 //mux control pins
 int s0 = 15;
@@ -12,120 +21,87 @@ int s3 = 31;
 //mux in "SIG" pin
 int SIG_pin = A3;
 
-//not correctly working digital output pins
-/*const int columnPins[5] = {
-  27, 30, 16, 6, 20
-};*/
-
-int readMux(int channel){
-  int controlPin[] = {s0, s1, s2, s3};
-
-  int muxChannel[16][4]={
-    {0,0,0,0}, //channel 0
-    {1,0,0,0}, //channel 1
-    {0,1,0,0}, //channel 2
-    {1,1,0,0}, //channel 3
-    {0,0,1,0}, //channel 4
-    {1,0,1,0}, //channel 5
-    {0,1,1,0}, //channel 6
-    {1,1,1,0}, //channel 7
-    {0,0,0,1}, //channel 8
-    {1,0,0,1}, //channel 9
-    {0,1,0,1}, //channel 10
-    {1,1,0,1}, //channel 11
-    {0,0,1,1}, //channel 12
-    {1,0,1,1}, //channel 13
-    {0,1,1,1}, //channel 14
-    {1,1,1,1}  //channel 15
-  };
-
-  //loop through the 4 sig
-  for(int i = 0; i < 4; i ++){
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
-  }
-
-  //read the value at the SIG pin
-  int val = analogRead(SIG_pin);
-
-  //return the value
-  return val;
-}
-
 void setup(){
   pinMode(s0, OUTPUT); 
   pinMode(s1, OUTPUT); 
   pinMode(s2, OUTPUT); 
   pinMode(s3, OUTPUT);
-  /*pinMode(27, OUTPUT);
-  pinMode(30, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(20, OUTPUT);*/
   pinMode(SIG_pin, INPUT); 
 
   digitalWrite(s0, LOW);
   digitalWrite(s1, LOW);
   digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
-  /*digitalWrite(27, LOW);
-  digitalWrite(30, LOW);
-  digitalWrite(16, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(20, LOW);*/
 
-  //initialize array with zeros
-  for(int i = 0; i < 16; i++){
-    for(int j = 0; j < 5; j++){
-      values[i][j] = 0;
-      }
-   }
-
+  msg.length = MAX_VALUES;
+  
   Serial.begin(115200);
+}
+
+/**
+ * analogRead through a 16 channel muxer
+ */
+uint16_t readMux(int channel) {
+  int controlPin[] = {s0, s1, s2, s3};
+
+  int muxChannel[ ROWS ][ 4 ] = {
+    {0, 0, 0, 0}, //channel 0
+    {1, 0, 0, 0}, //channel 1
+    {0, 1, 0, 0}, //channel 2
+    {1, 1, 0, 0}, //channel 3
+    {0, 0, 1, 0}, //channel 4
+    {1, 0, 1, 0}, //channel 5
+    {0, 1, 1, 0}, //channel 6
+    {1, 1, 1, 0}, //channel 7
+    {0, 0, 0, 1}, //channel 8
+    {1, 0, 0, 1}, //channel 9
+    {0, 1, 0, 1}, //channel 10
+    {1, 1, 0, 1}, //channel 11
+    {0, 0, 1, 1}, //channel 12
+    {1, 0, 1, 1}, //channel 13
+    {0, 1, 1, 1}, //channel 14
+    {1, 1, 1, 1} //channel 15
+  };
+
+  //loop through the 4 sig
+  for (int i = 0; i < 4; i ++) {
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  }
+
+  //read the value at the SIG pin
+  uint16_t val = analogRead(SIG_pin);
+
+  //return the value
+  return val;
+}
+
+void sendAll(unsigned char* str, uint16_t len){
+  Serial.write(str, len);  
 }
 
 void loop(){
   //read values
   duration = millis();
-  for(int i = 0; i < 5; i++){
+  int k = 0;
+  for(int i = 0; i < COLUMNS; i++){
     //digitalWrite(columnPins[i], HIGH);
-    for(int j = 0; j < 16; j++){
-      values[ j ][ i ] = readMux(j);
-      delay(1);
+    for(int j = 0; j < ROWS; j++){
+      msg.data[ k ] = readMux(j); //values[ j ][ i ];
+      // msg.data[ k ] = k; //values[ j ][ i ];
+      //print values
+      //Serial.print(msg.data[ k ]);
+      //Serial.print(" ");
+      k++;
     }
+    //Serial.println();
     //digitalWrite(columnPins[i], LOW);
   }
-  duration = millis() - duration;
-  
-  //print values on serial monitor
-  for(int i = 0; i < 16; i++){
-    for(int j = 0; j < 5; j++){
-      Serial.print(values[ i ][ j ]);
-      Serial.print("\t");
-    }
-    Serial.println();
-  }
-  Serial.print("duration = ");
-  Serial.print(duration);
-  Serial.println("ms ---------------------");
-  delay(300);
-  //write values via serial to processing
-      /*Serial.print (values[ i ][ j ], DEC );
-      if(j == 4){
-        Serial.print(";");
-      }
-      else{
-        Serial.print(",");
-      }
-    }
-    if(i == 15){
-      Serial.print("|");
-    }
-  }*/
+  //Serial.println("-------------------------------------------------");
 
-  //reset values to zero
-  for(int i = 0; i < 16; i++){
-    for(int j = 0; j < 5; j++){
-      values[i][j] = 0;
-      }
-   }
+  //transmit values
+  send_message(&msg);
+  for (uint16_t& val : msg.data) { //reset values to zero
+    val = 0;
+  }
+//  delay(200);
 }
