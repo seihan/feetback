09cde0e902a9fb8406c5191371bdf780b3984a5f
Hannes Seidel <greenredact@gmail.com>
2019-09-07 10:12:13 +0200

    add voltage divider for vbat on pin A6

--- Sensor/nrf52_read_vdd/nrf52_read_vdd.ino
+++ Sensor/nrf52_read_vdd/nrf52_read_vdd.ino
@@ -1,10 +1,42 @@
-uint32_t vdd_pin = 32;                // new imaginary pin
+int vdd_pin = 32;                // new imaginary pin
+int vbat_pin = A6;               // voltage divider
+#define V_MV_PER_LSB (0.87890625F)  // 3.6V ADC range and 12-bit ADC resolution = 3600mV/4096
+#define VBAT_DIVIDER      (0.705882353F)   // 1.8M + 0.750M voltage divider on VBAT = (1.8M / (0.750M + 1.8M))
+#define VBAT_DIVIDER_COMP (1.416666667F)   // Compensation factor for the VBAT divider ((0.750M + 1.8M) / 1.8M)
 
-#define VDD_MV_PER_LSB (0.87890625F)  // 3.6V ADC range and 12-bit ADC resolution = 3600mV/4096
+#define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * V_MV_PER_LSB)
+
 
 float readVDD(void) {
   float raw;
+  // Get the raw 12-bit, 0..3000mV ADC value
+  raw = analogRead(vdd_pin);
 
+  return raw * V_MV_PER_LSB;
+}
+
+float readVBAT(void) {
+  float raw;
+  raw = analogRead(vbat_pin);
+
+  return raw * REAL_VBAT_MV_PER_LSB;
+}
+
+uint8_t mvToPercent(float mvolts) {
+  if(mvolts<3300)
+    return 0;
+
+  if(mvolts <3600) {
+    mvolts -= 3300;
+    return mvolts/30;
+  }
+
+  mvolts -= 3600;
+  return 10 + (mvolts * 0.15F );  // thats mvolts /6.66666666
+}
+
+void setup() {
+  Serial.begin(115200);
   // Set the analog reference to 3.6V (default = 3.6V)
   analogReference(AR_INTERNAL);
 
@@ -14,19 +46,13 @@ float readVDD(void) {
   // Let the ADC settle
   delay(1);
 
-  // Get the raw 12-bit, 0..3000mV ADC value
-  raw = analogRead(vdd_pin);
-
-  return raw * VDD_MV_PER_LSB;
-}
-
-
-void setup() {
-  Serial.begin(115200);
   readVDD();
 }
 
 void loop() {
-  Serial.println( readVDD()) ;
+  Serial.println( "Internal voltage = " + String(readVDD()) + "mV" );
+  float vbat_mv = readVBAT();
+  Serial.println( "Battery voltage = " + String(vbat_mv) + "mV" );
+  Serial.println( "Battery level = " + String(mvToPercent(vbat_mv)) + "%" );
   delay(1000);
 }
