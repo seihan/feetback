e6768b4ca78bc4e18d58b97af78141df24076236
Hannes Seidel <greenredact@gmail.com>
2019-09-10 17:00:11 +0200

    add new struct balance

--- Sensor/fsr_sensor_board_ftb_periph/protocol.h
+++ Sensor/fsr_sensor_board_ftb_periph/protocol.h
@@ -19,10 +19,16 @@ struct measure_t {
   uint16_t value;
 };
 
+struct balance_t {
+  uint16_t front;
+  uint16_t rear;
+};
+
 struct message_t {
   unsigned char protocol_header[2] {PROTOCOL_HDR1, PROTOCOL_HDR2}; // data start marker
   uint16_t length; // number of measurements 
   measure_t data[MAX_VALUES];
+  balance_t bala[1];
 };
 
 void sendtoPeripherals(unsigned char* str, uint16_t len);
